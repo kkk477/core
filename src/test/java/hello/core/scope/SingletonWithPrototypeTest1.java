07db3accaba40cb9cd60e93bece714a6611927dc
HoWoong <dlghdnd@gmail.com>
2023-04-18 23:55:27 +0900

    웹 스코프까지 했음. request 스코프 예제 만들기 이어해야함.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -3,9 +3,12 @@ package hello.core.scope;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,30 +25,36 @@ public class SingletonWithPrototypeTest1 {
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
     }
 
-    @Test
-    void singletonClientUsePrototype() {
-        AnnotationConfigApplicationContext ac =
-                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
-
-        ClientBean clientBean1 = ac.getBean(ClientBean.class);
-        int count1 = clientBean1.logic();
-        assertThat(count1).isEqualTo(1);
-
-        ClientBean clientBean2 = ac.getBean(ClientBean.class);
-        int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
-    }
+//    @Test
+//    void singletonClientUsePrototype() {
+//        AnnotationConfigApplicationContext ac =
+//                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+//
+//        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+//        int count1 = clientBean1.logic();
+//        assertThat(count1).isEqualTo(1);
+//
+//        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+//        int count2 = clientBean2.logic();
+//        assertThat(count2).isEqualTo(1);
+//    }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        //private final PrototypeBean prototypeBean;
 
         @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+//        @Autowired
+//        ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
